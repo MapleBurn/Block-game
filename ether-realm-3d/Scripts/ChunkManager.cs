020693ea8e1429de93f14da4b72ccffdd8d36ad5
MapleBurn <miki.holan@outlook.cz>
2026-02-28 10:18:06 +0100

    chunker

--- ether-realm-3d/Scripts/ChunkManager.cs
+++ ether-realm-3d/Scripts/ChunkManager.cs
@@ -1,6 +1,7 @@
 using Godot;
 using System;
 using System.Threading;
+using System.Threading.Tasks;
 using Godot.Collections;
 using SCG = System.Collections.Generic;
 
@@ -13,11 +14,12 @@ public partial class ChunkManager : Node
     [Export] private Array<Color> _colors = new();
     [Export] private Vector3I _dimensions = new Vector3I(128, 64, 128);
     private readonly SCG.Dictionary<Vector3, Color> _data = new();
-    
+    private readonly SCG.Queue<Node> _addTreeNodes = new();
+
     private FastNoiseLite _noise = new();
     private Vector3 _chunkAmount;
     
-    private readonly SCG.List<Thread> _threads = new(4);
+    private readonly SCG.List<Task> _threads = new(4);
     
     private PackedScene _chunkScene;
     
@@ -26,13 +28,22 @@ public partial class ChunkManager : Node
         _chunkScene = GD.Load<PackedScene>("res://Scenes/chunk.tscn");
         _chunkAmount = _dimensions / _chunkSize;    // pro konečný svět spočítáme počet chunků
         
-        _threads.Add(new Thread(() => GenerateChunks(new Vector3(0,0,0))));
-        _threads.Add(new Thread(() => GenerateChunks(new Vector3(_dimensions.X / 2,0,0))));
-        _threads.Add(new Thread(() => GenerateChunks(new Vector3(0, 0, _dimensions.Z / 2))));
-        _threads.Add(new Thread(() => GenerateChunks(new Vector3(_dimensions.X / 2, 0, _dimensions.Z / 2))));
+        _threads.Add(Task.Run(() => GenerateChunks(new Vector3(0,0,0))));
+        _threads.Add(Task.Run(() => GenerateChunks(new Vector3(_dimensions.X / 2,0,0))));
+        _threads.Add(Task.Run(() => GenerateChunks(new Vector3(0, 0, _dimensions.Z / 2))));
+        _threads.Add(Task.Run(() => GenerateChunks(new Vector3(_dimensions.X / 2, 0, _dimensions.Z / 2))));
     }
 
-    public void GenerateChunks(Vector3 pos)
+    public Node GetNextReadyChunk()
+    {
+        if (_addTreeNodes.Count > 0)
+        {
+            return _addTreeNodes.Dequeue();
+        }
+        return null;
+    }
+    
+    private void GenerateChunks(Vector3 pos)
     {
         var chunks = _chunkAmount / 2;
         for (int x = 0; x < chunks.X; x++)
@@ -43,12 +54,18 @@ public partial class ChunkManager : Node
                 {
                     var chunk = _chunkScene.Instantiate<Chunk>();
                     chunk.Position = new Vector3(x, y, z) * _chunkSize + pos;
-                    AddChild(chunk);
                     chunk.GenerateData(_chunkSize, _dimensions.Y, _noise, _colors);
                     chunk.GenerateMesh();
+                    _addTreeNodes.Enqueue(chunk);
                 }
             }
         }
+    }
+
+    public override void _Process(double delta)
+    {
+        var ch = GetNextReadyChunk();
+        if (ch != null) AddChild(ch);
     }
 
     public override void _ExitTree()
--- ether-realm-3d/Scripts/WorldManager.cs
+++ ether-realm-3d/Scripts/WorldManager.cs
@@ -1,9 +1,4 @@
 using Godot;
-using System;
-using SCG = System.Collections.Generic;
-using System.Linq;
-using Godot.Collections;
-using Array = System.Array;
 
 namespace EtherRealm3D.Scripts;
 
@@ -13,7 +8,7 @@ public partial class WorldManager : Node3D
 	
 	public override void _Ready()
 	{
-		_chunkManager.GenerateChunks(new Vector3(0,0,0));
+		//_chunkManager.GenerateChunks(new Vector3(0,0,0));
 	}
 	
 	public override void _Process(double delta)
